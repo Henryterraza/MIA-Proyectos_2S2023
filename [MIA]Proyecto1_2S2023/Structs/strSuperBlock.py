f6ae8965e332c01f668f0838ebd9a71f118331b0
Henryterraza <henry.w.a.4@gmail.com>
2023-09-13 17:04:27 -0600

    Coreecion de superbloques

--- [MIA]Proyecto1_2S2023/Structs/strSuperBlock.py
+++ [MIA]Proyecto1_2S2023/Structs/strSuperBlock.py
@@ -1,77 +1,93 @@
 from abc import ABC, abstractmethod
 
-
 class Objeto(ABC):
     @abstractmethod
     def get_bytes(self):
         pass
-
     @abstractmethod
     def set_bytes(self, bytes):
-        pass
-
+        pass    
     @abstractmethod
     def get_size(self):
         pass
 
-
-class Inodo(Objeto):
+class SuperBlock(Objeto):
     # Constructor
-    def __init__(self, uid, gid, size, atime, ctime, mtime, type, perm):
-        self.uid = uid  # int
-        self.gid = gid  # int
-        self.size = size  # int
-        self.atime = atime  # int
-        self.ctime = ctime  # int
-        self.mtime = mtime  # int
-        self.type = type  # char[1]
-        self.perm = perm  # int
-        self.blocks = []  # lista[60]
-        for block in range(0, 15):
-            self.blocks.append(-1)
+    def __init__(self, filesystem_type, inodes_count , blocks_count, free_blocks_count, free_inodes_count, mtime, umtime, mnt_count, magic, inode_s, block_s, firts_ino,first_blo,bm_inode_start, bm_block_start,inode_start,block_start):
+        self.filesystem_type = filesystem_type          # int
+        self.inodes_count = inodes_count                # int
+        self.blocks_count = blocks_count                # int
+        self.free_blocks_count = free_blocks_count      # int
+        self.free_inodes_count = free_inodes_count      # int
+        self.mtime = mtime                              # int
+        self.umtime = umtime                            # int
+        self.mnt_count = mnt_count                      # int
+        self.magic = magic                              # int
+        self.inode_s = inode_s                          # int
+        self.block_s = block_s                          # int
+        self.firts_ino = firts_ino                      # int
+        self.first_blo = first_blo                      # int
+        self.bm_inode_start = bm_inode_start            # int
+        self.bm_block_start = bm_block_start            # int
+        self.inode_start = inode_start                  # int
+        self.block_start = block_start                  # int
 
     def get_bytes(self):
         bytes = bytearray()
-        bytes += self.uid.to_bytes(4, byteorder="big")
-        bytes += self.gid.to_bytes(4, byteorder="big")
-        bytes += self.size.to_bytes(4, byteorder="big")
-        bytes += self.atime.to_bytes(4, byteorder="big")
-        bytes += self.ctime.to_bytes(4, byteorder="big")
-        bytes += self.mtime.to_bytes(4, byteorder="big")
-        bytes += self.type.encode("utf-8").ljust(1, b"\x00")
-        bytes += self.perm.to_bytes(4, byteorder="big")
-
-        for block in self.blocks:
-            bytes += block.to_bytes(4, byteorder="big", signed=True)
-
+        bytes += self.filesystem_type.to_bytes(4, byteorder='big')      
+        bytes += self.inodes_count.to_bytes(4, byteorder='big')           
+        bytes += self.blocks_count.to_bytes(4, byteorder='big')           
+        bytes += self.free_blocks_count.to_bytes(4, byteorder='big')      
+        bytes += self.free_inodes_count.to_bytes(4, byteorder='big')      
+        bytes += self.mtime.to_bytes(4, byteorder='big')                  
+        bytes += self.umtime.to_bytes(4, byteorder='big')                 
+        bytes += self.mnt_count.to_bytes(4, byteorder='big')              
+        bytes += self.magic.to_bytes(4, byteorder='big')                  
+        bytes += self.inode_s.to_bytes(4, byteorder='big')                
+        bytes += self.block_s.to_bytes(4, byteorder='big')                
+        bytes += self.firts_ino.to_bytes(4, byteorder='big')              
+        bytes += self.first_blo.to_bytes(4, byteorder='big')              
+        bytes += self.bm_inode_start.to_bytes(4, byteorder='big')         
+        bytes += self.bm_block_start.to_bytes(4, byteorder='big')         
+        bytes += self.inode_start.to_bytes(4, byteorder='big')            
         return bytes
 
     def set_bytes(self, bytes):
-        self.uid = int.from_bytes(bytes[0:4], byteorder="big")
-        self.gid = int.from_bytes(bytes[4:8], byteorder="big")
-        self.size = int.from_bytes(bytes[8:12], byteorder="big")
-        self.atime = int.from_bytes(bytes[12:16], byteorder="big")
-        self.ctime = int.from_bytes(bytes[16:20], byteorder="big")
-        self.mtime = int.from_bytes(bytes[20:24], byteorder="big")
-        self.type = bytes[24:25].decode("utf-8").rstrip("\x00")
-        self.perm = int.from_bytes(bytes[25:29], byteorder="big")
-        self.blocks = []
-
-        sick = 4
-        for i in range(0, 15):
-            self.blocks.append(
-                int.from_bytes(
-                    bytes[25 + sick : 29 + sick], byteorder="big", signed=True
-                )
-            )
-            sick += 4
-
+        self.filesystem_type = int.from_bytes(bytes[0:4], byteorder='big')      
+        self.inodes_count = int.from_bytes(bytes[4:8], byteorder='big')
+        self.blocks_count = int.from_bytes(bytes[8:12], byteorder='big')
+        self.free_blocks_count = int.from_bytes(bytes[12:16], byteorder='big')
+        self.free_inodes_count = int.from_bytes(bytes[16:20], byteorder='big')
+        self.mtime = int.from_bytes(bytes[20:24], byteorder='big') 
+        self.umtime = int.from_bytes(bytes[24:28], byteorder='big') 
+        self.mnt_count = int.from_bytes(bytes[28:32], byteorder='big')
+        self.magic = int.from_bytes(bytes[32:36], byteorder='big')                  
+        self.inode_s = int.from_bytes(bytes[36:40], byteorder='big') 
+        self.block_s = int.from_bytes(bytes[40:44], byteorder='big')
+        self.firts_ino = int.from_bytes(bytes[44:48], byteorder='big')
+        self.first_blo = int.from_bytes(bytes[48:52], byteorder='big')
+        self.bm_inode_start = int.from_bytes(bytes[52:56], byteorder='big')
+        self.bm_block_start = int.from_bytes(bytes[56:60], byteorder='big')
+        self.inode_start = int.from_bytes(bytes[60:64], byteorder='big')
         return self
 
     def get_size(self):
         size = 0
-        size += 4 * 7
-        size += 1
-        size += 4 * 15
-
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
+        size += 4
         return size
+        
